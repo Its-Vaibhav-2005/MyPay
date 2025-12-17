b9470f6a8c523780bf4f79ef215ba4dc7eee447c
Its-Vaibhav-2005 <pandey.vaibhav311005@gmail.com>
2025-12-17 23:49:45 +0530

    Backend Complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import {initDB} from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import job from './config/cron.js';
 
 import transactionRoutes from './routes/transactionsRoutes.js';
 
@@ -10,7 +12,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// start cron job
+if(process.env.NODE_ENV === 'production'){
+  job.start();
+}
+
 // middleware
+app.use(cors()); // Enable CORS for all origins
 app.use(express.json());
 app.use(rateLimiter)
 
